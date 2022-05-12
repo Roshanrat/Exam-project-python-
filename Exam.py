c45133aa8ba07ad2f4c97d0bbd063042bdeb7921
Roshan Rathnayake <104135983+Roshanrat@users.noreply.github.com>
2022-05-12 22:48:37 +0530

    Started exam analysis project

---
+++ Exam.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # INTRODUCTION
+# 
+# ### To know which factor may affect the student's performance, we classify the score into couple of ranks, and figure out which feature affects the score more significant. The independent variables are follow:
+# 
+# ### gender                                  : sex of students
+# ### race/ethnicity                       : ethnicity of students
+# ### parental level of education : parents' final education level
+# ### lunch                                     : get their lunch before the exam
+# ### test preparation course      : complete or not complete before test
+
+# ## Import packages
+
+# In[26]:
+
+
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt 
+
